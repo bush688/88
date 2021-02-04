02c61bf422303c6bf0e8663ddb2767b882217935
bush688 <32064695+bush688@users.noreply.github.com>
2021-02-04 22:43:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+ Modify default IP
+sed -i 's/192.168.2.1/192.168.2.1/g' package/base-files/files/bin/config_generate
